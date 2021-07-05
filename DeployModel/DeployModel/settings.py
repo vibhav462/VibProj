66757097079ec6f1fb076ac647f832e51e0c5b0b
vibhav462 <69111281+vibhav462@users.noreply.github.com>
2021-07-05 10:21:33 +0530

    commit

--- DeployModel/DeployModel/settings.py
+++ DeployModel/DeployModel/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(ct7f4!3m3t7z%8l+%5z3%i)trxgp0y59m7f6!c!a=a4*betes'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- DeployModel/DeployModel/urls.py
+++ DeployModel/DeployModel/urls.py
@@ -18,10 +18,14 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('',views.upload, name= 'upload'),
     path('admin', admin.site.urls),
     path('about',views.about,name='about'),
-    path('contact',views.contact,name='contact')
+    path('contact',views.contact,name='contact'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
